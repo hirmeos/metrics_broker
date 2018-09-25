e32a94ed219e587c4584ee0f7fba57130dae65e8
Javier Arias <javier@arias.re>
2018-09-25 10:16:21 +0100

    rename token

--- src/e2e/logout.e2e.js
+++ src/e2e/logout.e2e.js
@@ -14,8 +14,8 @@ describe('Logout', () => {
   beforeEach(async () => {
     page = await browser.newPage();
     await page.goto(startUrl, { waitUntil: 'networkidle2' });
-    await page.evaluate(() => {
-      window.localStorage.setItem('metrics-broker-token', token);
+    await page.evaluate((t) => {
+      window.localStorage.setItem('metrics-broker-token', t);
     }, token);
     await page.evaluate(() =>
       window.localStorage.setItem('metrics-broker-authority', 'admin')
