a2e0b93a35f63e78e58ea07629463f029aaee10e
Javier Arias <javier@arias.re>
2018-09-26 15:19:04 +0100

    translate comment

--- config/plugin.config.js
+++ config/plugin.config.js
@@ -5,7 +5,7 @@ import AntDesignThemePlugin from 'antd-pro-theme-webpack-plugin';
 import path from 'path';
 
 export default config => {
-  // 将所有 less 合并为一个供 themePlugin使用
+  // Merge all less files
   const outFile = path.join(__dirname, '../.temp/ant-design-pro.less');
   const stylesDir = path.join(__dirname, '../src/');
 
