188538974f77f0aa3c15161b3a46ac9f56121734
Javier Arias <javier@arias.re>
2018-09-26 10:19:08 +0100

    update dependencies

--- src/e2e/dashboard.e2e.js
+++ src/e2e/dashboard.e2e.js
@@ -14,7 +14,7 @@ describe('Dashboard', () => {
   beforeEach(async () => {
     page = await browser.newPage();
     await page.goto(startUrl, { waitUntil: 'networkidle2' });
-    await page.evaluate((t) => {
+    await page.evaluate(t => {
       window.localStorage.setItem('metrics-broker-token', t);
     }, token);
     await page.evaluate(() =>
